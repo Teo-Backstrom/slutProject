8062e6d8de37c0653564c9c882af1793c006f753
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-05-25 18:24:07 +0200

    gjort AI för datorskott

--- slutProject/Program.cs
+++ slutProject/Program.cs
@@ -10,6 +10,9 @@ namespace sänkaSkepp
         static int kartBredd = 100;
         static int kartHöjd = 100;
 
+        //switch på hur datorn ska sjuta
+        static int val = 0;
+
         //skapar kartor
         static string[,] spelarensKarta = new string[kartBredd, kartHöjd];
         static string[,] datornsKarta = new string[kartBredd, kartHöjd];
@@ -23,7 +26,12 @@ namespace sänkaSkepp
         static string[] spelInformation = new string[4];
 
         //namn på fil för att används i flera funktioner
-        static string filnamn = "centralbord.csv";
+        static string filnamn = "sankaskepp.csv";
+
+
+        //datorns skott position för användning av AI
+        static int datorSkottX = 0;
+        static int datorSkottY = 0;
 
 
 
@@ -69,6 +77,7 @@ namespace sänkaSkepp
                         //kör metoder för att spela spelet
                         SkapaKartorna();
                         PlaceraDubbelSkepp();
+                        PlaceraTrippelSkepp();
                         SpelaSänkaSkepp();
                         break;
                     case 2:
@@ -146,7 +155,7 @@ namespace sänkaSkepp
         /// </summary>
         static void PlaceraSkepp()
         {
-
+            //sätter antalet skepp från inställningsfilen
             int antal = int.Parse(spelInformation[1]);
             //loopar igenom tills det inte finns fler skepp kvar
             for (int i = 0; i < antal; i++)
@@ -212,7 +221,7 @@ namespace sänkaSkepp
                     Console.WriteLine("PS. kom ihåg att kordinaterana utgår från 4je kvadranten");
                     int y = ReadInt(kartHöjd, 1);
                     //kolla om stället är ledigt
-                    if (spelarensKarta[x, y] == "O" && spelarensKarta[x + 1, y] == "O")
+                    if (spelarensKarta[x - 1, y - 1] == "O" && spelarensKarta[x, y - 1] == "O")
                     {
                         spelarensKarta[x - 1, y - 1] = "X";
                         spelarensKarta[x, y - 1] = "X";
@@ -234,7 +243,7 @@ namespace sänkaSkepp
                     int x = slump.Next(kartBredd);
                     int y = slump.Next(kartHöjd);
                     //kollar om pos är ledig för skeppet annars så slumpas en ny pos fram
-                    if (datornsKarta[x, y] == "O" && datornsKarta[x + 1, y] == "O")
+                    if (spelarensKarta[x, y] == "O" && spelarensKarta[x + 1, y] == "O")
                     {
                         datornsKarta[x, y] = "X";
                         datornsKarta[x + 1, y] = "X";
@@ -244,6 +253,244 @@ namespace sänkaSkepp
 
             }
         }
+
+        static void PlaceraTrippelSkepp()
+        {
+            bool flagga = false;
+            while (flagga == false)
+            {
+                //låter användare ange x och y pos för skepp
+                Console.WriteLine($"Vart vill du ställa ut ditt vertikala trippelskepp utifrån dens övre position");
+                Console.WriteLine($"X-kordinat välj mellan 1-{kartBredd}");
+                Console.WriteLine("PS. kom ihåg att kordinaterana utgår från 4je kvadranten");
+                int xPosition = ReadInt(kartBredd, 1);
+                Console.WriteLine($"Y-kordinat välj mellan 1-{kartHöjd}");
+                Console.WriteLine("PS. kom ihåg att kordinaterana utgår från 4je kvadranten");
+                int yPosition = ReadInt(kartHöjd - 2, 1);
+                //kolla om stället är ledigt
+                if (spelarensKarta[xPosition - 1, yPosition - 1] == "O" && spelarensKarta[xPosition - 1, yPosition] == "O" && spelarensKarta[xPosition - 1, yPosition + 1] == "O")
+                {
+                    spelarensKarta[xPosition - 1, yPosition - 1] = "X";
+                    spelarensKarta[xPosition - 1, yPosition] = "X";
+                    spelarensKarta[xPosition - 1, yPosition + 1] = "X";
+                    flagga = true;
+                }
+                else
+                {
+                    Console.WriteLine("Finns redan ett skepp på samma ställe, försök igen");
+                }
+            }
+
+            bool flagga2 = false;
+            while (flagga2 == false)
+            {
+                //slupar pos för datorns skepp
+                int xPosition = slump.Next(kartBredd);
+                int yPosition = slump.Next(kartHöjd);
+                //kollar om pos är ledig för skeppet annars så slumpas en ny pos fram
+                if (datornsKarta[xPosition, yPosition] == "O" && datornsKarta[xPosition, yPosition + 1] == "O" && datornsKarta[xPosition, yPosition + 2] == "O")
+                {
+                    datornsKarta[xPosition, yPosition] = "X";
+                    datornsKarta[xPosition, yPosition + 1] = "X";
+                    datornsKarta[xPosition, yPosition + 2] = "X";
+                    flagga2 = true;
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        /// Smartare sätt hur datorn väljer att sjuta
+        /// </summary>
+        static void datorAI()
+        {
+            //switch på vilket håll som datorn ska sjuta här näst om den fått en träff
+            switch (val)
+            {
+                //om ingen träff så är det random på en plats som är ledig
+                case 0:
+                    bool flagga = true;
+                    while (flagga)
+                    {
+                        datorSkottX = slump.Next(kartBredd);
+                        datorSkottY = slump.Next(kartHöjd);
+                        // om den redan är skjuten på så skjut inte
+                        if (datornsSkott[datorSkottX, datorSkottY] == true)
+                        {
+
+                        }
+                        else
+                        {
+                            datornsSkott[datorSkottX, datorSkottY] = true;
+                            //om träff börja med nästa steg på skjutprocessen
+                            if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                            {
+                                val = 1;
+                            }
+                            flagga = false;
+                        }
+                    }
+                    break;
+                //börja skjut till höger
+                case 1:
+                    datorSkottX++;
+                    // om det är innan för planen gör detta annars gör nästa steg
+                    if (datorSkottX < kartBredd)
+                    {
+                        datornsSkott[datorSkottX, datorSkottY] = true;
+                        //om träffa gör samma igen om miss gå till nästa steg
+                        if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                        {
+                            val = 1;
+                        }
+                        else
+                        {
+                            datorSkottX--;
+                            val = 2;
+                        }
+
+                    }
+                    else
+                    {
+                        datorSkottX = datorSkottX - 2;
+                        datornsSkott[datorSkottX, datorSkottY] = true;
+                        //om träffa gör samma sak igen annars gör nästa steg
+                        if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                        {
+                            val = 2;
+                        }
+                        else
+                        {
+                            datorSkottX++;
+                            val = 3;
+                        }
+                    }
+
+                    break;
+                //skjut till vänster om träffen
+                case 2:
+                    datorSkottX--;
+                    //om innanför planen annars gör nästa steg
+                    if (datorSkottX > 0)
+                    {
+                        datornsSkott[datorSkottX, datorSkottY] = true;
+                        //om träff gör samma igen annars gå till nästa steg
+                        if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                        {
+                            val = 2;
+                        }
+                        else
+                        {
+                            datorSkottX++;
+                            val = 3;
+                        }
+
+                    }
+                    else
+                    {
+                        datorSkottY++;
+                        //om innan för planen annars gör nästa steg
+                        if (datorSkottY < kartHöjd)
+                        {
+                            datornsSkott[datorSkottX, datorSkottY] = true;
+                            //om träffa gör samma sak annars gör nästa steg
+                            if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                            {
+                                val = 3;
+                            }
+                            else
+                            {
+                                datorSkottY--;
+                                val = 4;
+                            }
+                        }
+                        else
+                        {
+                            datorSkottY = datorSkottY - 2;
+                            datornsSkott[datorSkottX, datorSkottY] = true;
+                            //om träffa gör samma sak igen
+                            if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                            {
+                                val = 4;
+                            }
+                            else
+                            {
+                                val = 0;
+                            }
+                        }
+
+
+                    }
+                    break;
+                //skjuta nedåt
+                case 3:
+                    datorSkottY++;
+                    //om innanför planen annars gör nästa steg
+                    if (datorSkottY < kartHöjd)
+                    {
+                        datornsSkott[datorSkottX, datorSkottY] = true;
+                        //om träff gör samma sak igen annars gör nästa steg
+                        if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                        {
+                            val = 3;
+                        }
+                        else
+                        {
+                            datorSkottY--;
+                            val = 4;
+                        }
+
+                    }
+                    else
+                    {
+                        datorSkottY--;
+                        datornsSkott[datorSkottX, datorSkottY] = true;
+                        //om träff gör samma sak igen annars gå tillbaka till första grundtillståndet
+                        if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true)
+                        {
+                            val = 4;
+                        }
+                        else
+                        {
+                            datorSkottY++;
+                            val = 0;
+                        }
+
+                    }
+                    break;
+                //skjuta uppåt
+                case 4:
+                    //om innnanför planenn annars skjut random till nästa träff
+                    if (datorSkottY - 1 > 0)
+                    {
+                        datorSkottY--;
+                        datornsSkott[datorSkottX, datorSkottY] = true;
+                        //om träfa gör samma sak igen
+                        if (spelarensKarta[datorSkottX, datorSkottY] == "X" && datornsSkott[datorSkottX, datorSkottY] == true && datorSkottY > 0)
+                        {
+                            val = 4;
+                        }
+                        else
+                        {
+                            val = 0;
+                        }
+                    }
+                    else
+                    {
+                        datorSkottX = slump.Next(kartBredd);
+                        datorSkottY = slump.Next(kartHöjd);
+                        datornsSkott[datorSkottX, datorSkottY] = true;
+                        val = 0;
+                    }
+
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
 
         /// <summary>
         /// Spelets huvudloop
@@ -262,9 +509,10 @@ namespace sänkaSkepp
                 Console.WriteLine("Var vill du skjuta? (Y)");
                 int y = ReadInt(kartHöjd, 1);
                 spelarensSkott[x - 1, y - 1] = true;
-                datornsSkott[slump.Next(kartBredd), slump.Next(kartHöjd)] = true;
+                datorAI();
+                // datornsSkott[slump.Next(kartBredd), slump.Next(kartHöjd)] = true; (den orginella varianten) som inte har några problem
 
-                //om spelare vunnit så informeras det och det lagras i fil och slutar matchen efter valfri knapp tryckt
+                //om spelare vunnit så informeras det och det lagras i fil och slutar matchen efter valfri knapp tryckt och val åtrställ till 0
                 if (HarSpelarenVunnit())
                 {
                     Console.Clear();
@@ -274,8 +522,9 @@ namespace sänkaSkepp
                     File.WriteAllLines(filnamn, spelInformation);
                     Console.ReadKey();
                     harNågonVunnit = true;
+                    val = 0;
                 }
-                //om datorn vunnit så informeras det och det lagras i fil och slutar matchen efter valfri knapp tryckt
+                //om datorn vunnit så informeras det och det lagras i fil och slutar matchen efter valfri knapp tryckt och val åtrställ till 0
                 else if (HarDatornVunnit())
                 {
                     Console.Clear();
@@ -285,16 +534,17 @@ namespace sänkaSkepp
                     File.WriteAllLines(filnamn, spelInformation);
                     Console.ReadKey();
                     harNågonVunnit = true;
+                    val = 0;
 
                 }
             }
         }
 
 
+
         /// <summary>
         /// Ritar ut spelarens och datorns kartor
         /// </summary>
-
         static void RitaSpelplanen()
         {
             Console.WriteLine("Spelarens karta");
@@ -302,6 +552,7 @@ namespace sänkaSkepp
             {
                 for (int x = 0; x < kartBredd; x++)
                 {
+                    //om datorn skjutit så är märket rött
                     if (datornsSkott[x, y] == true)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
@@ -322,7 +573,7 @@ namespace sänkaSkepp
             {
                 for (int x = 0; x < kartBredd; x++)
                 {
-                    // Endast rutor som spelaren har skjutit på syns
+                    // Endast rutor som spelaren har skjutit på syns rött av datorns karta annars "-"
                     if (spelarensSkott[x, y] == true)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
@@ -347,17 +598,19 @@ namespace sänkaSkepp
         /// <returns></returns>
         static bool HarSpelarenVunnit()
         {
-
+            //loopar igenom hala datorns karta och kollar om spelaren skutit där
             for (int y = 0; y < kartHöjd; y++)
             {
                 for (int x = 0; x < kartBredd; x++)
                 {
+                    //om inte träffas så false
                     if (datornsKarta[x, y] == "X" && spelarensSkott[x, y] == false)
                     {
                         return false;
                     }
                 }
             }
+            //om spelarens skott är true på alla skepp så return true
 
             return true;
         }
@@ -368,6 +621,8 @@ namespace sänkaSkepp
         /// <returns></returns>
         static bool HarDatornVunnit()
         {
+            //loopar igenom hala spelarens karta och kollar om datorn skutit där
+
             for (int y = 0; y < kartHöjd; y++)
             {
                 for (int x = 0; x < kartBredd; x++)
@@ -378,6 +633,7 @@ namespace sänkaSkepp
                     }
                 }
             }
+            //om spelarens skott är true på alla skepp så return true
 
             return true;
         }
